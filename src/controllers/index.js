83420e2aa4ba6c37ceff5a4ec4cfa1cf18c1fc07
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2024-12-05 12:20:43 -0300

    Rework return values of Posts.js model and handleAgregarPost

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,25 +2,25 @@ const Posts = require('../models/Posts');
 
 const handleGetPosts = async (req, res) => {
     const posts = await Posts.getAll();
-    res.status(200).json(posts);
+    res.json(posts);
 };
 
 const handleAgregarPost = async (req,res) => {
-    const {titulo, url, descripcion} = req.body;
-    await Posts.add(titulo, url, descripcion);
-    res.status(201).send('Post agregado');
+    const {titulo, url, img, descripcion} = req.body;
+    const post = await Posts.add(titulo, url || img, descripcion);
+    res.json(post);
 }
 
 const handleLike = async (req, res) => {
     const {id} = req.params;
     await Posts.like(id);
-    res.status(200).send('Like realizado');
+    res.send('Like realizado');
 }
 
 const handleEliminarPost = async (req, res) => {
     const {id} = req.params;
     await Posts.remove(id);
-    res.status(200).send('Post eliminado');
+    res.send('Post eliminado');
 }
 
 module.exports = {
